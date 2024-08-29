cfba8056a543fafc0ed37278ad81169c1e453194
geekLoop3r <hipolito.alba.c@gmail.com>
2024-08-28 20:34:43 -0500

    Constuctor de persona

--- egg/IntroPOO/Application.java
+++ egg/IntroPOO/Application.java
@@ -3,15 +3,22 @@ package egg.IntroPOO;
 public class Application {
     public static void main(String[] args) {
         // personas del mundo real representadas en objetos
-        Persona persona1 = new Persona(); // instanciar
+        Persona persona1 = new Persona(); // instancias
+        persona1.nombre = "Sandra";
         persona1.edad = 23; 
 
-        Persona michael = new Persona(); 
-        michael.nombre = "Michael";
-        michael.edad = 18;
+        Persona persona2 = new Persona(); 
+        persona2.nombre = "Michael";
+        persona2.edad = 18;
 
-        // persona1.imprimirNombreYEdad();
-        // michael.imprimirNombreYEdad();
+        Persona persona3 = new Persona("Tatiana", 12);
+        Persona persona4 = new Persona("René", 34);
+
+        //Usando Métodos
+        persona1.imprimirNombreYEdad();
+        persona2.imprimirNombreYEdad();
+        persona3.imprimirNombreYEdad();
+        persona4.imprimirNombreYEdad();
 
         Auto auto1 = new Auto("Ferrary", "Enzo", 2002);
         Auto auto2 = new Auto("Chevrolet", "Corvette");
--- egg/IntroPOO/Persona.java
+++ egg/IntroPOO/Persona.java
@@ -4,9 +4,19 @@ public class Persona {
     // atributos, propiedades o caracteristicas
     String nombre;
     Integer edad;
+    static Integer numero = 0; 
 
     // metodos, comportamientos o acciones
     public void imprimirNombreYEdad() {
         System.out.println("Me llamo " + nombre + " y tengo " + edad + " años.");
     }
+
+    public Persona(){};
+
+    public Persona(String nombre, Integer edad){
+        this.nombre = nombre;
+        this.edad = edad;
+        numero++;
+        System.out.println("Número de persona registrada" + numero);
+    };
 }
